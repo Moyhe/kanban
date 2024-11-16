6848a44d87311fb91ad49cc87f6e710691e5b1e1
Mohye <dragonsinmeliodas777@gmail.com>
2024-11-16 15:24:05 +0200

    design the databse schema for members

---
+++ app/Http/Requests/StoreMemeberRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMemeberRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string'],
+            'title' => ['required', 'string'],
+            'age' => ['required', 'integer'],
+            'email' => ['required', 'email', 'unique:members,email'],
+            'mobile_number' => ['required', 'string', 'regex:/^\+?[1-9]\d{1,14}$/']
+        ];
+    }
+}

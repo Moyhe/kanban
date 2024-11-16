a9f09b4d6413568692191599c3bec919bba23f2f
Mohye <dragonsinmeliodas777@gmail.com>
2024-11-16 16:33:28 +0200

    adding rotues for members

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\MemeberController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::apiResource('members', MemeberController::class);
